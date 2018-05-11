448a17a5d9494e6d344f26ce39e0c37b5383182f
enzotrompeneers <trompeneers@telenet.be>
2018-05-11 12:17:02 +0200

    portfolio routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
-Route::get('/', function () {
-    return view('home.index');
+Route::get('/', 'HomeController@index');
+
+Route::resource('portfolio', 'PortfolioController');
+Route::get('zoeken', 'PortfolioController@index');
+
+Route::get('registreren', function () {
+    return view('partials.register');
 });
