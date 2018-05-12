79c9ef098b7f1e6a81d7d3e457d5f9504dc3d5f4
enzotrompeneers <trompeneers@telenet.be>
2018-05-12 20:27:53 +0200

    adding method addReview

--- app/Portfolio.php
+++ app/Portfolio.php
@@ -4,14 +4,18 @@ namespace App;
 
 class Portfolio extends Model
 {
-    public function Ratings()
+    public function reviews()
     {
-        //return $this->hasMany('Rating', 'rating_id');
-        return $this->hasMany(Rating::class);
+        return $this->hasMany(Review::class);
     }
 
     public static function getAll()
     {
         return static::latest()->get();
+    }
+
+    public function addReview($rating, $body)
+    {
+        $this->reviews()->create(compact('rating', 'body'));
     }
 }
