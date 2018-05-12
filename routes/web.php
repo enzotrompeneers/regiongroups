ff235259edad463f01ea1963b9d32a6286db30a0
enzotrompeneers <trompeneers@telenet.be>
2018-05-12 20:32:35 +0200

    specific routes for portfolios and reviews

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 Route::get('/', 'HomeController@index');
 
-Route::resource('portfolio', 'PortfolioController');
+//Route::resource('portfolios', 'PortfolioController');
+Route::get('portfolios', 'PortfolioController@index');
+Route::get('portfolios/nieuw', 'PortfolioController@create');
+Route::post('portfolios', 'PortfolioController@store');
+Route::get('portfolios/{portfolio}', 'PortfolioController@show');
+
+Route::post('portfolios/{portfolio}/reviews', 'ReviewController@store');
+
 Route::get('zoeken', 'PortfolioController@index');
 
 Route::get('registreren', function () {
