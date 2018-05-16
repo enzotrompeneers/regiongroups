a8fee96252191c21e8e35766197389aa4d08d288
enzotrompeneers <trompeneers@telenet.be>
2018-05-16 13:53:23 +0200

    auth navigation

---
+++ resources/views/layouts/web/navigation.blade.php
@@ -0,0 +1,25 @@
+<div class="navigation">
+    <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="">Over ons</a></li>
+        <li><a href="{{ route('search') }}">Zoeken</a></li>
+        <li><a href="">Contacteren</a></li>
+        @auth
+            <li><a href="">{{ Auth::user()->name }}</a></li>
+            <li><a href="{{ route('portfolio.create') }}">Nieuw portfolio</a></li>
+            <li>
+                <a href="{{ route('logout') }}" 
+                onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                    Uitloggen
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+        @else
+            <li><a href="{{ route('login') }}">Inloggen</a></li>
+            <li><a href="{{ route('register') }}">Registreren</a></li>
+        @endauth
+    </ul>
+</div>
+        
