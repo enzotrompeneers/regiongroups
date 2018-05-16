ef9ccd271b344403d82d111d57eaf8981c38e570
enzotrompeneers <trompeneers@telenet.be>
2018-05-16 13:54:31 +0200

    show portfolio with review and review form

--- resources/views/portfolios/show.blade.php
+++ resources/views/portfolios/show.blade.php
@@ -1,8 +1,28 @@
 @extends('layouts.web.master')
 
 @section('content')
-    <h1>{{ $portfolio->name }}</h1>
-    <h2>{{ $portfolio->description }}</h2>
+    <h2>{{ $portfolio->name }}</h3>
+    <p>
+        Logo: <br>
+        {{ $portfolio->logo }}
+    </p>
+    <p>
+        Beschrijving: <br>
+        {{ $portfolio->description }}
+    </p>
+
+    <p>
+        Adres: <br>
+        {{ $portfolio->street }} {{ $portfolio->housenumber }}, <br>
+        {{ $portfolio->postal_code }} {{ $portfolio->city }} {{ $portfolio->country }}
+    </p>
+
+    <p>
+        GSM/Tel: {{ $portfolio->phone }} <br>
+        Email: {{ $portfolio->email }} <br>
+        Website: {{ $portfolio->external }}
+    </p>
+    <p>Gemaakt door: {{$portfolio->user->name}}, op: {{ $portfolio->created_at->toFormattedDateString() }}</p>
 
     <hr>
     <h3>Reviews</h3>
@@ -10,25 +30,31 @@
         <p>
             Rating:{{ $review->rating }} <br>
             Review: {{ $review->body }} <br>
-            {{ $review->created_at->diffForHumans() }}
+            Gemaakt door: {{$review->user->name}} {{ $review->created_at->diffForHumans() }}
         </p>
     @endforeach
 
     <hr>
+
     <h3>Review plaatsen</h3>
-    <form action="/portfolios/{{ $portfolio->id }}/reviews" method="post">
-        {{ csrf_field() }}
+    @auth
+        <form action="/portfolios/{{ $portfolio->id }}/reviews" method="post">
+            {{ csrf_field() }}
 
-        <label for="rating">Rating</label>
-        <input type="text" id="rating" name="rating" placeholder="Rating" required>
-        <br>
+            <label for="rating">Rating</label>
+            <input type="text" id="rating" name="rating" placeholder="Rating" required>
+            <br>
 
-        <label for="body">Review</label>
-        <textarea name="body" id="body" required></textarea>
-        <br>
+            <label for="body">Review</label>
+            <textarea name="body" id="body" required></textarea>
+            <br>
 
-        <button type="submit">Review toevoegen</button>
-    </form>
+            <button type="submit">Review toevoegen</button>
+        </form>
+        @include('partials.errors')
 
-    @include('partials.errors')
+    @else
+        <p>Om een review te plaatsen moet je <a href="{{route('login')}}">inloggen</a>.</p>
+
+    @endauth
 @endsection
