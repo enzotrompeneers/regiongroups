eec0ba719d3a95eddae56df82952e740c53cebef
enzotrompeneers <trompeneers@telenet.be>
2018-05-17 18:55:16 +0200

    validation rules for portfolio

--- app/Http/Requests/PortfolioRequest.php
+++ app/Http/Requests/PortfolioRequest.php
@@ -24,17 +24,17 @@ class PortfolioRequest extends FormRequest
     public function rules()
     {
         return [
-            'logo' => 'nullable|max:255|image',
-            'name' => 'required|string|max:255',
+            'logo' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'name' => 'required|max:30',
             'description' => 'required|min:10',
-            'street' => 'required|string|max:255',
-            'housenumber' => 'required|string|max:255',
-            'postal_code' => 'required|string|min:4|max:255',
-            'city' => 'required|string|max:255',
-            'country' => 'required|string|max:255',
-            'phone' => 'required|string|min:3|max:255',
-            'email' => 'required|string|email|max:255',
-            'external' => 'nullable|max:255|active_url'
+            'street' => 'required|string|max:30',
+            'housenumber' => 'required|max:10',
+            'postal_code' => 'required|min:4|max:8',
+            'city' => 'required|max:30',
+            'country' => 'required|max:30',
+            'phone' => 'required|min:3|max:20',
+            'email' => 'required|email|max:30',
+            'external' => 'nullable|active_url|max:30'
         ];
     }
 }
