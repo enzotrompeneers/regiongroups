bf7d26e31f4c6ac662addd6b240bc49621fd5e20
enzotrompeneers <trompeneers@telenet.be>
2018-05-26 15:52:27 +0200

    show old search else current city

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -3,7 +3,7 @@
     <label for="search">Wat zoek je?</label>
     <input type="text" id="search" name="search" placeholder="Zoeken" value="{{ isset($search)? $search : '' }}">
     <label for="city">Gemeente</label>
-    <input type="text" id="city" name="city" placeholder="Gemeente" value="{{ isset($city)? $city : $city_name }}">
+    <input type="text" id="city" name="city" placeholder="Gemeente" value="{{ old('city', isset($city)? $city : $city_name) }}">
 
     <button type="submit">Zoeken</button>
 </form>
