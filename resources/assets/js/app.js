3961fb6105a16d57cc2e0e91d17756bf971716ac
enzotrompeneers <trompeneers@telenet.be>
2018-05-29 22:52:19 +0200

    Initalize foundation

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,14 @@
 import Notification from './components/Notification';
 
+require('./foundation');
+$(document).foundation();
 
-require('./bootstrap');
 window.Vue = require('vue');
 require('vue-resource');
 
-Vue.http.interceptors.push((request, next) => { 
-    request.headers.set('X-CSRF-TOKEN', Cityofcompanies.csrfToken); 
-    next(); 
-});
+Vue.http.interceptors.push(function(request) {
+    request.method = 'POST';
+    request.headers.set('X-CSRF-TOKEN', Cityofcompanies.csrfToken);
+  });
 
 Vue.component('CheckoutForm', require('./components/CheckoutForm.vue'));
-
-
-
