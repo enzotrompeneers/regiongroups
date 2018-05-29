39c96be34b6a1ad9d56476914276dce6f2b53559
enzotrompeneers <trompeneers@telenet.be>
2018-05-29 22:56:59 +0200

    checkout details

--- resources/assets/js/components/CheckoutForm.vue
+++ resources/assets/js/components/CheckoutForm.vue
@@ -2,7 +2,7 @@
     <form action="/betaling" method="POST">
         <input type="hidden" name="stripeToken" v-model="stripeToken">
         <input type="hidden" name="stripeEmail" v-model="stripeEmail">
-        <button type="submit" @click.prevent="buy">Abonneer</button>
+        <button type="submit" @click.prevent="buy">Abonneer voor 1 jaar</button>
     </form>
 </template>
 
@@ -21,7 +21,6 @@
                 key: Cityofcompanies.stripeKey,
                 image: "https://stripe.com/img/documentation/checkout/marketplace.png",
                 locale: "auto",
-                currency: "eur",
                 token: (token) => {
                     this.stripeToken = token.id;
                     this.stripeEmail = token.email;
@@ -35,9 +34,11 @@
         methods: {
             buy() {
                 this.stripe.open({
-                    name: "Abonneer voor 1 jaar",
-                    description: "Professional Version",
-                    amount: 5000,
+                    name: 'Abonneer voor 1 jaar',
+                    description: 'Pro versie',
+                    zipCode: true,
+                    currency: 'eur',
+                    amount: 4999
                 });
             }
         }
