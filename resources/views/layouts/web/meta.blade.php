4043cc1818625596ea63289c1fe6d262e128d349
enzotrompeneers <trompeneers@telenet.be>
2018-05-29 22:55:54 +0200

    vue post request csrf token

--- resources/views/layouts/web/meta.blade.php
+++ resources/views/layouts/web/meta.blade.php
@@ -1,4 +1,12 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <title>Regiongroups</title>
+
+<script>
+    var Cityofcompanies = {
+        csrfToken: "{{ csrf_token() }}",
+        stripeKey: "{{ config('services.stripe.key') }}"
+    };
+</script>
