222efe9caa822a8476aea0d90b625926322d67a5
enzotrompeneers <trompeneers@telenet.be>
2018-06-10 23:15:40 +0200

    search design

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,9 +1,33 @@
-<form action="{{ route('home') }}" method="get">
+<form class="search" action="{{ route('home') }}" method="get">
     @csrf
-    <label for="search">Wat zoek je?</label>
-    <input type="text" id="search" name="search" placeholder="Zoeken" value="{{ isset($search)? $search : '' }}">
-    <label for="city">Gemeente</label>
-    <input type="text" id="city" name="city" placeholder="Gemeente" value="{{ old('city', isset($city)? $city : $city_name) }}">
+        <div class="control has-addon">
+            <span class="control-addon radius-top-left radius-bottom-left">
+                Wat zoek je?
+            </span>
+            <input class="control-field" type="text" id="search" name="search" placeholder="Zoeken..." value="{{ isset($search)? $search : '' }}">
+            <input class="control-field" type="text" id="city" name="city" placeholder="Gemeente" value="{{ old('city', isset($city)? $city : $city_name) }}">
+            <button type="submit" class="control-addon radius-top-right radius-bottom-right">
+                <i class="fa fa-search"></i>
+            </button>
+        </div>
+        
+        {{-- <div class="control has-addon">
+            <span class="control-addon addon-left radius-bottom-left">
+                Welke gemeente?
+            </span>
+            <input class="control-field" type="text" id="city" name="city" placeholder="Gemeente" value="{{ old('city', isset($city)? $city : $city_name) }}">
+            <button type="submit" class="control-addon radius-right">
+                <i class="fa fa-search"></i>
+            </button>
+        </div> --}}
 
-    <button type="submit">Zoeken</button>
+        {{-- <div class="cell medium-5">
+            <label for="city">Gemeente</label>
+            
+            <input type="text" id="city" name="city" placeholder="Gemeente" value="{{ old('city', isset($city)? $city : $city_name) }}">
+
+        </div>
+        <div class="cell medium-2">
+            <button type="submit" class="button">Zoeken</button>
+        </div> --}}
 </form>
