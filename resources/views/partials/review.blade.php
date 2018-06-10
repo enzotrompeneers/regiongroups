2b915ff7c4af8c70be459f50aa90047dfee66713
enzotrompeneers <trompeneers@telenet.be>
2018-06-10 23:16:02 +0200

    review functionality

---
+++ resources/views/partials/review.blade.php
@@ -0,0 +1,17 @@
+<form class="review" action="/portfolios/{{ $portfolio->name }}/reviews" method="post">
+    @csrf
+    <div class="grid-x">
+        <div class="cell">
+            <label for="rating">Rating</label>
+            <input type="text" id="rating" value="{{ old('rating') }}" name="rating" placeholder="Rating" required>
+        </div>
+        <div class="cell">
+            <label for="body">Review</label>
+            <textarea name="body" id="body" required>{{ old('body') }}</textarea>
+        </div>
+        <div class="cell">
+            <button type="submit" class="button">Review toevoegen</button>
+        </div>
+    </div>
+</form>
+@include('partials.errors')
