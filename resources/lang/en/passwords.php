98a8f26479c4ccd1324f9e964b2066a863bec64e
enzotrompeneers <trompeneers@telenet.be>
2018-06-10 23:05:51 +0200

    dutch translations and - validation

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -1,22 +1,9 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
-
+    'password' => 'Wachtwoord moeten uit minimaal zes tekens bestaan ​​en overeenkomen met het bevestigingswachtwoord.',
+    'reset' => 'Je wachtwoord is gereset!',
+    'sent' => 'We hebben uw link voor het opnieuw instellen van uw wachtwoord per e-mail verzonden!',
+    'token' => 'Dit wachtwoordherstel-token is ongeldig.',
+    'user' => 'We kunnen geen gebruiker met dat e-mailadres vinden.',
 ];
