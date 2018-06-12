b09ba1f56a24bb97cabd5fe8fbb8c1c90fe2933a
enzotrompeneers <trompeneers@telenet.be>
2018-06-12 20:42:49 +0200

    merge particles

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,16 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'js')
+    .scripts([
+        'resources/assets/js/particles.js',
+        'resources/assets/js/particles-config.js'
+    ], 'public/js/all-particles.js')
+
    .sass('resources/assets/sass/app.scss', 'css')
+
    .browserSync({
         proxy: 'regiongroups.test/',
         port: 8000
     })
+
     .version();
