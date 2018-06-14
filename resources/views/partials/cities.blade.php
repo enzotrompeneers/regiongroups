98b4a598fc1a7f13d897e193aaf9dad02da58dfe
enzotrompeneers <trompeneers@telenet.be>
2018-06-14 22:07:25 +0200

    cities alphabetical with letter category

--- resources/views/partials/cities.blade.php
+++ resources/views/partials/cities.blade.php
@@ -1,6 +1,27 @@
-<ul>
-    <li><a href="{{ route('home') }}">Alles</a></li>
-    @foreach($cities as $city)
-    <li><a href="{{ route('city.show', $city->name) }}">{{ $city->name }}: {{ $city->amount }}</a></li>
-    @endforeach
-</ul>
+<section class="tag-cloud-section">
+    <h5 class="tag-cloud-title">Vind via een gemeente</h5>
+    <div class="tag-cloud">
+        <a class="tag-cloud-individual-tag" href="{{ route('home') }}">Alles tonen</a>
+            <?php
+            $isMatch = false;
+            foreach(range('A', 'Z') AS $letter)
+            {   
+                foreach( $cities->sortBy('name') AS $city )
+                {
+                    if(strtoupper(substr($city->name, 0, 1)) ===  $letter)
+                    {
+                        if(!$isMatch) {
+                            $isMatch = true;
+                            echo "<p>" . $letter . "</p>";
+                        }
+                        ?> 
+                        <a class="tag-cloud-individual-tag" href="{{ route('city.show', $city->name) }}">{{ $city->name }}: {{ $city->amount }}</a>
+                        <?php
+                    }
+                }
+                $isMatch = false;
+            }
+            ?>
+        </div>
+    </div>
+</section>
